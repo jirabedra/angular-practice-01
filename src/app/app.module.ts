34d96e2958ba7d45da751003147adf1e26d7e281
jirabedra-embarkvet <juan.irabedra@embarkvet.com>
2023-10-23 18:03:47 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HorizontalNavBarComponent } from './components/horizontal-nav-bar/horizontal-nav-bar.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorizontalNavBarComponent,
+    MoviesComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/movies/movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent {
+  constructor(private movieService : MovieService){}
+}
